81f4c5d35ce169f56575cbadf3bc555dc2e14ad1
Alireza Khodakarami <jiraiyahsama@yahoo.com>
2016-04-24 08:03:42 +0430

    Works :D

--- src/main/java/jiraiyah/deathcounter/DeathEventHandler.java
+++ src/main/java/jiraiyah/deathcounter/DeathEventHandler.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2016 DeathCounter (Jiraiyah)
+ *
+ * project link :
+ *
+ * Licensed under The MIT License (MIT);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jiraiyah.deathcounter;
 
 import net.minecraft.scoreboard.IScoreCriteria;
@@ -17,6 +34,19 @@ public class DeathEventHandler
         ScoreObjective objective = world.getScoreboard().addScoreObjective("deaths", IScoreCriteria.DEATH_COUNT);
         objective.setDisplayName("Deaths");
         world.getScoreboard().setObjectiveInDisplaySlot(0, objective);
+    }
+
+    @SubscribeEvent
+    public void onWordGenerateSpawnPoint( WorldEvent.Load event)
+    {
+        World world = event.getWorld();
+        ScoreObjective objective = world.getScoreboard().getObjective("deaths");
+        if (objective == null)
+        {
+            objective = world.getScoreboard().addScoreObjective("deaths", IScoreCriteria.DEATH_COUNT);
+            objective.setDisplayName("Deaths");
+            world.getScoreboard().setObjectiveInDisplaySlot(0, objective);
+        }
     }
 }
 
